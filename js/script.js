aa798e2d6809441637f234bc3548cfdd53ad848f
Natalie Gillam <ngillam@hotmail.co.uk>
2023-05-30 16:43:44 +0100

    added functionality to fetch repo data

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-// Div where profile information will appear.
+// select div where profile information will appear.
 const overview = document.querySelector(".overview")
 
-// GitHub username
+// variable for GitHub username
 const userName = "Nadia982"
+
+// select the unordered list to display the repos list
+const repoList = document.querySelector(".repo-list")
 
 // async function to fetch info from GitHub profile
 async function fetchUser(){
@@ -31,4 +34,28 @@ const displayUserInfo = function(data) {
   </div> 
     `
     overview.append(div)
+    gitRepos()
 }
+
+// Fetch Repos
+const gitRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${userName}/repos?sort=updated&per_page=100&affiliation=owner`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+  };
+
+  // Display Info About Repos
+const displayRepos = function(repos){
+for (const repo of repos){
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo")
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`
+    repoList.append(repoItem)
+}
+}
+
+
+// Save and view the page. Under your profile information, you should now see your repos beginning with the 
+//repo you’ve updated most recently (see screenshot above).
+// Add and commit your changes with the command line. 
+//Push the changes up to GitHub. Copy the link to your repo and submit it below.
