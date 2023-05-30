fa43a409ecb44c5e8bd249194d07d52d17a8013c
Natalie Gillam <ngillam@hotmail.co.uk>
2023-05-30 16:11:57 +0100

    added functionality to fetch user data

--- js/script.js
+++ js/script.js
@@ -4,3 +4,31 @@ const overview = document.querySelector(".overview")
 // GitHub username
 const userName = "Nadia982"
 
+// async function to fetch info from GitHub profile
+async function fetchUser(){
+    const userInfo = await fetch(`https://api.github.com/users/${userName}`)
+    console.log(userInfo)
+    const data = await userInfo.json();
+    console.log(data)
+    displayUserInfo(data)
+}
+
+fetchUser()
+
+// Fetch & Display User Information
+const displayUserInfo = function(data) {
+    const div = document.createElement("div")
+    div.classList.add("user-info");
+    div.innerHTML = `
+    <figure>
+    <img alt="photo of Natalie Gillam" src=${data.avatar_url} />
+    </figure>
+  <div>
+    <p><strong>Name:</strong> ${data.name}</p>
+    <p><strong>Bio:</strong> ${data.bio}</p>
+    <p><strong>Location:</strong> ${data.location}</p>
+    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+  </div> 
+    `
+    overview.append(div)
+}
